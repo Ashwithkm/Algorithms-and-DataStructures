05a189786b31a3606ad0934d28b9c248156e450f
Ashwithkm <78860496+Ashwithkm@users.noreply.github.com>
2021-06-18 12:23:39 +0530

    Create MaxAverageSum.py

---
+++ MaxAverageSum.py
@@ -0,0 +1,14 @@
+#arr[] = {1, 12, -5, -6, 50, 3} , k=4
+
+arr = [1,12,-5,-6,50,3]
+k = 4
+result = []
+for i in range(len(arr)-k):
+    sum = 0
+    for j in range (i,i+k):
+        sum += arr[j]
+    result.append(sum/k)
+print(result)
+       
+#O(n) = n*k -> time complexity
+#O(1) -> space complexity
